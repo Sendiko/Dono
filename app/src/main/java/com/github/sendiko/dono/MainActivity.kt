17d5c03007347462aa5497a43b9949bdecb06d9d
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:44:52 +0700

    feat(login): initiate viewmodel, call the screen

--- app/src/main/java/com/github/sendiko/dono/MainActivity.kt
+++ app/src/main/java/com/github/sendiko/dono/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.sendiko.dono.login.LoginScreen
+import com.github.sendiko.dono.login.LoginViewModel
 import com.github.sendiko.dono.ui.theme.DonoTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DonoTheme {
-
+                val viewModel = viewModel<LoginViewModel>()
+                LoginScreen(viewModel)
             }
         }
     }
