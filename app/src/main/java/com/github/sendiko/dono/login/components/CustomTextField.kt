16247d1b73da6801710386cbf0a9560aad6d5ace
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:08:13 +0700

    feat(add): Column for CustomTextField

--- app/src/main/java/com/github/sendiko/dono/login/components/CustomTextField.kt
+++ app/src/main/java/com/github/sendiko/dono/login/components/CustomTextField.kt
@@ -1,5 +1,9 @@
 package com.github.sendiko.dono.login.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
