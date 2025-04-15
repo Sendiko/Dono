46d0041eea6379137d063042d9b5d80610722f0c
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:06:18 +0700

    feat(add): CustomTextField file

---
+++ app/src/main/java/com/github/sendiko/dono/login/components/CustomTextField.kt
@@ -0,0 +1,9 @@
+package com.github.sendiko.dono.login.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomTextField(modifier: Modifier = Modifier) {
+
+}
