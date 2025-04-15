1afcd109b80b1f8106c121530b61c37f49601ac5
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:11:43 +0700

    feat(add): welcoming text

--- app/src/main/java/com/github/sendiko/dono/login/LoginScreen.kt
+++ app/src/main/java/com/github/sendiko/dono/login/LoginScreen.kt
@@ -2,16 +2,31 @@ package com.github.sendiko.dono.login
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
 
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-
+        Text(
+            text = "Login",
+            fontSize = 24.sp
+        )
+        Text(
+            text = "Login menggunakan username dan password anda.",
+        )
     }
 
 }
