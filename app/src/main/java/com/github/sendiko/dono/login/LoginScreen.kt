00898a793ff8a5e5ca4bce5d70a3d5696943131a
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:05:17 +0700

    feat(add): Column on LoginScreen

--- app/src/main/java/com/github/sendiko/dono/login/LoginScreen.kt
+++ app/src/main/java/com/github/sendiko/dono/login/LoginScreen.kt
@@ -1,9 +1,17 @@
 package com.github.sendiko.dono.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
 
 }
