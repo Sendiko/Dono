8ddc6252288410b7554a7e8d428e3898a7239744
Sendiko <sarangtawon897@gmail.com>
2025-04-15 11:38:49 +0700

    feat: hoist state

--- app/src/main/java/com/github/sendiko/dono/login/LoginViewModel.kt
+++ app/src/main/java/com/github/sendiko/dono/login/LoginViewModel.kt
@@ -1,6 +1,13 @@
 package com.github.sendiko.dono.login
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class LoginViewModel: ViewModel() {
+
+    private val _username = MutableStateFlow("")
+    val username: StateFlow<String> = _username.asStateFlow()
+
 }
